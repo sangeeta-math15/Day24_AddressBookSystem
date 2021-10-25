1f66151c15ee0b0d1bed221e185f3bf09b59a5d3
sngtmth <sngtmth@gmail.com>
2021-10-26 01:20:48 +0530

    Added:Create a Contact in AddressBook

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBook {
+    Scanner sc=new Scanner(System.in);
+    ArrayList<Contacts> list=new ArrayList<>();
+}
--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome into the Address Book System ");
+        AddressBook addressBook=new AddressBook();
     }
 }
